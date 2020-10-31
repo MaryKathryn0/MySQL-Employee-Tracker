8a0bea839f7f2abe044fdde561f1b86be93f71bf
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-10-31 16:40:38 -0500

    added to database to include roles and employees to test

--- database/emptrack_db.sql
+++ database/emptrack_db.sql
@@ -6,30 +6,35 @@ USE emptrack_db;
 
 CREATE TABLE department (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  namedept VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-INSERT INTO department (namedept)
-VALUES ("hr");
+INSERT INTO department (name)
+VALUES ("HR"),("IT"),("Sales");
+
 
 CREATE TABLE role (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
-  department_id INT NOT NULL 
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id) 
 );
 
 INSERT INTO role (title,salary,department_id)
-VALUES ("Director", "100000", "1234");
+VALUES ("Director of HR", 100000, 1), ("Trainer", 75000, 1), ("Director of IT", 100000, 2), ("Admin", 50000, 2),
+("Director of Sales", 100000, 3), ("Sales Rep", 50000, 3);
 
 CREATE TABLE employee (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INTEGER NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES ("Mary", "Jane", "1000", "1111");
+VALUES ("Mary", "Jane", 1, NULL), ("John", "Doe", 3, NULL), ("Bilbo", "Baggins", 4, 2), ("Snoop", "Dog", 2, 1), ("Lois", "Lane", 5, NULL), ("Casper", "Ghost", 6, 5);
 
--- server.js
+++ server.js
@@ -103,7 +103,7 @@ function addRole() {
             name: "salary"
         },
         {
-            type: "input",
+            type: "list",
             message: "Input department id:",
             name: "department_id"
         }
@@ -141,14 +141,7 @@ function addEmpl() {
 }
 
 // function viewDept() {
-//     let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
-//     connection.query(query, function (err, res) {
-//         function(err, res) {
-//             if(err) throw err
-//             console.table(res)
-//             startPrompt()
-//         }
-//     });
+    
 
 // }
 
@@ -178,14 +171,8 @@ function addEmpl() {
 // }
 
 // function viewEmpl() {
-//     let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
-//     connection.query(
-//     function(err, res) {
-//         if (err) throw err;
-//         console.table(res)
-//         startPrompt();
-//     }
-// })
+//}
+
 
 
 
