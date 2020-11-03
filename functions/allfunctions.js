dbdc77ace456aa6855cbae0707f51c0367d1e102
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-11-02 22:05:53 -0600

    worked on readme and changed some menu options to delete the bonus options

--- functions/allfunctions.js
+++ functions/allfunctions.js
@@ -1,9 +1,9 @@
 const inquirer = require("inquirer");
 
 const choices = ["View All Employees", "View All Employees by Department", "View All Employees by Manager",
-    "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager",
+    "Add Employee", "Update Employee Role", 
     "View All Roles", "Add Role", "Remove Role",
-    "View All Departments", "Add Department", "Remove Department", "Exit"];
+    "View All Departments", "Add Department", "Exit"];
 
 function askMainMenu() {
     return inquirer.prompt([{
